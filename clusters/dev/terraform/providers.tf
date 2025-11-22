2f47706fa57900f6c0a3862624e7977bbf9eebaf
SuperJugger88 <68207376+SuperJugger88@users.noreply.github.com>
2025-11-22 22:23:57 +0300

    test: changed providers

--- clusters/dev/terraform/providers.tf
+++ clusters/dev/terraform/providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     kubernetes = {
-      source  = "hashicorp/kubernetes"
+      source  = "opentofu/kubernetes"
       version = "2.23.0"
     }
     helm = {
-      source  = "hashicorp/helm"
+      source  = "opentofu/helm"
       version = "2.11.0"
     }
   }
