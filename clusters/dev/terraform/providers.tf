0429058d3de1d324e21f179185341b0d0fbebd44
SuperJugger88 <68207376+SuperJugger88@users.noreply.github.com>
2025-11-22 23:11:55 +0300

    test: fixing network mirror

--- clusters/dev/terraform/providers.tf
+++ clusters/dev/terraform/providers.tf
@@ -14,14 +14,15 @@ terraform {
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "docker-desktop"
-  host = "https://localhost:6443"
-
+  host = "https://127.0.0.1:6443"
+  insecure = true
 }
 
 provider "helm" {
   kubernetes {
     config_path    = "~/.kube/config"
     config_context = "docker-desktop"
-    host           = "https://localhost:6443"
+    host           = "https://127.0.0.1:6443"
+    insecure = true
   }
 }
