55adc522c2fc996bbea430ea01fd6a1c9b491231
SuperJugger88 <68207376+SuperJugger88@users.noreply.github.com>
2025-11-23 12:18:35 +0300

    fix: deleted opentofu registry

--- clusters/dev/terraform/providers.tf
+++ clusters/dev/terraform/providers.tf
@@ -1,24 +1,22 @@
 terraform {
   required_providers {
     kubernetes = {
-      source  = "registry.opentofu.org/hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.23.0"
     }
     helm = {
-      source  = "registry.opentofu.org/hashicorp/helm"
+      source  = "hashicorp/helm"
       version = "2.11.0"
     }
   }
 }
 
-# Правильная in-cluster конфигурация для Kubernetes
 provider "kubernetes" {
   host                   = "https://kubernetes.default.svc:443"
   token                  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")
   cluster_ca_certificate = file("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
 }
 
-# Правильная in-cluster конфигурация для Helm
 provider "helm" {
   kubernetes {
     host                   = "https://kubernetes.default.svc:443"
